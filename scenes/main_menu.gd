ff7645183aa0182eec1c51d81c2a7daae84dc1e7
Cristina Rits <crystal@tlu.ee>
2025-05-02 21:35:18 +0300

    music + non working options

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,15 +1,15 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -17,7 +17,13 @@ func _on_start_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	print("pressed")
+	main_buttons.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
