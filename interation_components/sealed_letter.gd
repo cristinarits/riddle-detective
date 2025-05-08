e742d4e0cf3c4fc8999f890760abe901c1667091
Cristina Rits <crystal@tlu.ee>
2025-05-08 19:36:32 +0300

    you can pick the letter

--- interation_components/sealed_letter.gd
+++ interation_components/sealed_letter.gd
@@ -3,6 +3,13 @@ class_name SealedLetter
 @export var interact_name: String = "(E)"
 @export var is_interactable: bool = true
 
+@export var item: InvItem
+
 var interact: Callable = func():
-	print("You picked up the sealed letter.")
-	queue_free()
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.collect(item)
+		print("✅ You picked up the sealed letter.")
+		queue_free()
+	else:
+		print("❌ Player not found!")
