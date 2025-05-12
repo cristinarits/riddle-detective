6f77b87a7b820923bd1bc6556dc2f0c6fd5c47dc
German Vesi <gervesi@users.noreply.github.com>
2025-05-12 04:19:03 +0300

    Clue fix and notebook added
    Tab - notebook

--- mira.gd
+++ mira.gd
@@ -15,8 +15,8 @@ var start_pos
 var is_roaming = true
 var is_chatting = false
 
-@export var note_title := "New note"
-@export_multiline var note_content := "New clue and information"
+@export var note_title := "New clue"
+@export_multiline var note_content := "Lia is known by Mira, Direction: Academy of Aetheria"
 @export var add_to_journal := true
 
 func _ready():
