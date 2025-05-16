d2ba25c5ea85f2bbf6b828276c8444e2cc604513
cristina. <crystal@tlu.ee>
2025-05-16 22:02:02 +0300

    add items

---
+++ burnt_parchment.gd
@@ -0,0 +1,21 @@
+extends Area2D
+class_name Parchment
+@export var interact_name: String = "Burnt Parchment"
+@export var is_interactable: bool = true
+
+@export var item: InvItem
+
+var interact: Callable = func():
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.collect(item)
+
+		var note_scene = preload("res://UI/note_ui.tscn")
+		print("Note scene loaded: ", note_scene != null)
+		var note = note_scene.instantiate()
+		get_tree().root.add_child(note)
+		note.open("Found Item", "To see like her, I named the stars in her honor… Aurelia, always looking up.")
+		
+		queue_free()
+	else:
+		print("Player not found!")
